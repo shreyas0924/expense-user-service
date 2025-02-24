6571b009046e169caa418cf35a3e96aa17a3886a
shreyas0924 <shreyasp0924@gmail.com>
2025-02-24 15:59:00 +0530

    updated env and entity scan

--- src/main/java/com/expense/userservice/UserserviceApplication.java
+++ src/main/java/com/expense/userservice/UserserviceApplication.java
@@ -2,10 +2,12 @@ package com.expense.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = { "com.expense.userservice.entities" })
 @EnableJpaRepositories(basePackages = { "com.expense.userservice.repository" })
 @ComponentScan(basePackages = { "com.expense.userservice.consumer", "com.expense.userservice.service",
 		"com.expense.userservice.config" })
