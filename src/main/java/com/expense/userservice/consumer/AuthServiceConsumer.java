99ab578e3f902427c53ed0b5a346687e5118bbb4
shreyas0924 <shreyasp0924@gmail.com>
2025-02-24 15:26:35 +0530

    update env

--- src/main/java/com/expense/userservice/consumer/AuthServiceConsumer.java
+++ src/main/java/com/expense/userservice/consumer/AuthServiceConsumer.java
@@ -1,13 +1,9 @@
 package com.expense.userservice.consumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jms.AcknowledgeMode;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
-
 import com.expense.userservice.entities.UserInfoDto;
-import com.expense.userservice.repository.UserRepository;
 import com.expense.userservice.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -24,11 +20,11 @@ public class AuthServiceConsumer {
      private ObjectMapper objectMapper;
 
      @KafkaListener(topics = "${spring.kafka.topic-json.name}", groupId = "${spring.kafka.consumer.group-id}")
-     public void listen(UserInfoDto eventData, Acknowledgment acknowledgment) {
+     public void listen(UserInfoDto eventData) {
           try {
-               System.out.println("AuthServiceConsumer: Consumed event: " + eventData);
-               // userService.createOrUpdateUser(eventData);
-               acknowledgment.acknowledge();
+               // Todo: Make it transactional, to handle idempotency and validate email,
+               // phoneNumber etc
+               userService.createOrUpdateUser(eventData);
           } catch (Exception ex) {
                ex.printStackTrace();
                System.out.println("AuthServiceConsumer: Exception is thrown while consuming kafka event");
